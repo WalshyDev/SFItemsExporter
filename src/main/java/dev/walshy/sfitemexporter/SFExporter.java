e1180d7799fa47b33e27e3060aba0b4eea08ab8c
Daniel Walsh <walshydev@gmail.com>
2020-04-25 22:46:15 +0100

    ItemSettings API

--- src/main/java/dev/walshy/sfitemexporter/SFExporter.java
+++ src/main/java/dev/walshy/sfitemexporter/SFExporter.java
@@ -1,11 +1,12 @@
 package dev.walshy.sfitemexporter;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
 import io.github.thebusybiscuit.slimefun4.core.attributes.Radioactive;
-import io.github.thebusybiscuit.slimefun4.core.services.localization.Language;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.SlimefunPlugin;
 import org.bukkit.Bukkit;
@@ -20,62 +21,78 @@ import java.io.IOException;
 
 public class SFExporter extends JavaPlugin {
 
+    private final Gson gson = new Gson();
+
     @Override
     public void onEnable() {
-        getServer().getScheduler().runTaskLaterAsynchronously(this, () -> {
-            JsonArray root = new JsonArray();
+        try {
+            exportItems();
+        } catch (Exception e) {
+            // In case there's some stupid thing happening
+            e.printStackTrace();
+        }
+        Bukkit.shutdown();
+    }
 
-            for (SlimefunItem item : SlimefunPlugin.getRegistry().getAllSlimefunItems()) {
-                JsonObject itemObj = new JsonObject();
-                itemObj.addProperty("id", item.getID());
-                itemObj.add("item", itemToJson(item.getItem()));
-                itemObj.addProperty("category", item.getCategory().getUnlocalizedName());
-                itemObj.addProperty("categoryTier", item.getCategory().getTier());
+    private void exportItems() {
+        JsonArray root = new JsonArray();
 
-                if (item.getRecipeType() != null && item.getRecipe() != null) {
-                    itemObj.addProperty("recipeType", item.getRecipeType().getKey().toString());
+        for (SlimefunItem item : SlimefunPlugin.getRegistry().getAllSlimefunItems()) {
+            getLogger().info("Exporting " + item.getID());
 
-                    JsonArray recipe = new JsonArray();
-                    for (ItemStack is : item.getRecipe()) {
-                        if (is != null)
-                            recipe.add(itemToJson(is));
-                        else
-                            recipe.add(JsonNull.INSTANCE);
-                    }
-                    itemObj.add("recipe", recipe);
+            JsonObject itemObj = new JsonObject();
+            itemObj.addProperty("id", item.getID());
+            itemObj.add("item", itemToJson(item.getItem()));
+            itemObj.addProperty("category", item.getCategory().getUnlocalizedName());
+            itemObj.addProperty("categoryTier", item.getCategory().getTier());
+
+            final JsonObject itemSettings = new JsonObject();
+            for (ItemSetting<?> setting : item.getItemSettings()) {
+                itemSettings.addProperty(setting.getKey(), String.valueOf(setting.getDefaultValue()));
+            }
+            itemObj.add("settings", itemSettings);
+
+            if (item.getRecipeType() != null && item.getRecipe() != null) {
+                itemObj.addProperty("recipeType", item.getRecipeType().getKey().toString());
+
+                JsonArray recipe = new JsonArray();
+                for (ItemStack is : item.getRecipe()) {
+                    if (is != null)
+                        recipe.add(itemToJson(is));
+                    else
+                        recipe.add(JsonNull.INSTANCE);
                 }
-
-                if (item.getResearch() != null) {
-                    itemObj.addProperty("research", item.getResearch().getKey().toString());
-                    itemObj.addProperty("researchCost", item.getResearch().getCost());
-                }
-                itemObj.addProperty("useableInWorkbench", item.isUseableInWorkbench());
-                item.getWikipage().ifPresent(link -> itemObj.addProperty("wikiLink", link));
-
-                itemObj.addProperty("electric", item instanceof EnergyNetComponent);
-                if (item instanceof EnergyNetComponent) {
-                    EnergyNetComponent component = (EnergyNetComponent) item;
-                    itemObj.addProperty("electricType", component.getEnergyComponentType().toString());
-                    itemObj.addProperty("electricCapacity", component.getCapacity());
-                }
-
-                itemObj.addProperty("radioactive", item instanceof Radioactive);
-                if (item instanceof Radioactive)
-                    itemObj.addProperty("radioactivityLevel",
-                        ((Radioactive) item).getRadioactivity().toString());
-
-                root.add(itemObj);
+                itemObj.add("recipe", recipe);
             }
 
-            try (FileWriter fw = new FileWriter(new File("output.json"))) {
-                fw.write(root.toString());
-                fw.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (item.getResearch() != null) {
+                itemObj.addProperty("research", item.getResearch().getKey().toString());
+                itemObj.addProperty("researchCost", item.getResearch().getCost());
+            }
+            itemObj.addProperty("useableInWorkbench", item.isUseableInWorkbench());
+            item.getWikipage().ifPresent(link -> itemObj.addProperty("wikiLink", link));
+
+            itemObj.addProperty("electric", item instanceof EnergyNetComponent);
+            if (item instanceof EnergyNetComponent) {
+                EnergyNetComponent component = (EnergyNetComponent) item;
+                itemObj.addProperty("electricType", component.getEnergyComponentType().toString());
+                itemObj.addProperty("electricCapacity", component.getCapacity());
             }
 
-            Bukkit.shutdown();
-        }, 5 * 20);
+            itemObj.addProperty("radioactive", item instanceof Radioactive);
+            if (item instanceof Radioactive)
+                itemObj.addProperty("radioactivityLevel",
+                    ((Radioactive) item).getRadioactivity().toString());
+
+            root.add(itemObj);
+        }
+
+        try (FileWriter fw = new FileWriter(new File("output.json"))) {
+            fw.write(root.toString());
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private JsonObject itemToJson(final ItemStack is) {
